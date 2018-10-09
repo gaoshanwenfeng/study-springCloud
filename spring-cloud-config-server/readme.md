69be7bcc61224963085bbef62a151de58de7d06b
高峰 <376221986@qq.com>
2018-10-09 14:40:04 +0800

    修改

---
+++ spring-cloud-config-server/readme.md
@@ -0,0 +1,2 @@
+1 测试 Server 端是否可以读取到 github 上面的配置信息 浏览器访问地址  http://localhost:7030/config-client/dev
+    如果直接查看配置文件中的配置信息可访问: http://localhost:7030/config-client-dev.yml
--- spring-cloud-config-server/src/main/java/com/myapp/configserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/myapp/configserver/SpringCloudConfigServerApplication.java
@@ -2,11 +2,13 @@ package com.myapp.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class HotelSoaConsumerFeignHystrixTurbineApplication {
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelSoaConsumerFeignHystrixTurbineApplication.class, args);
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 }
